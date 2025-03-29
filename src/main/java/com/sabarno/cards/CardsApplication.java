79a3e38404ea7745b9b414773b98db0e6f8d9422
Sabarno-15102002 <sabarnobiswas2002@gmail.com>
2025-03-29 11:29:24 +0530

    EasyBank Cards Microservice Created

---
+++ src/main/java/com/sabarno/cards/CardsApplication.java
@@ -0,0 +1,30 @@
+package com.sabarno.cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "EazyBank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Sabarno Biswas",
+						email = "sabarnobiswas2002@gmail.com"
+				)
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+
+}
